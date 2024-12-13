acc651f823669ff110a9f827455f0b290d4e208e
유종열 <114922306+jongyeol2@users.noreply.github.com>
2024-12-13 09:46:32 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.py
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.py
@@ -1,6 +1,2 @@
 def solution(num_list, n):
-    answer = []
-    for i in range(len(num_list)):
-        if i+1 >= n:
-            answer.append(num_list[i])
-    return answer
+    return num_list[n-1:]
