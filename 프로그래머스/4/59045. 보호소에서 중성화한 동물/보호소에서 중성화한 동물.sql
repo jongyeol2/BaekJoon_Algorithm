f459e8f4f0af1e18a0c163c1083a15d88afc53ab
유종열 <114922306+jongyeol2@users.noreply.github.com>
2024-12-16 09:20:02 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,8 @@
+select ai.animal_id,
+       ai.animal_type,
+       ai.name
+from animal_ins ai
+left join animal_outs ao on ai.animal_id = ao.animal_id
+where ai.sex_upon_intake like 'Intact%'
+  and ao.sex_upon_outcome not like 'Intact%'
+order by 1
