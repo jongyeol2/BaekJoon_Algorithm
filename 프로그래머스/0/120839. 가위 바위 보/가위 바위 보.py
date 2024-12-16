9b1f120cae74d7b1c77b6f511354cd194b821f77
유종열 <114922306+jongyeol2@users.noreply.github.com>
2024-12-16 10:15:11 +0900

    [level 0] Title: 가위 바위 보, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,10 +1,18 @@
 def solution(rsp):
-    answer = ''
-    for i in rsp:
-        if i == '2':
-            answer += '0'
-        elif i == '0':
-            answer += '5'
-        elif i == '5':
-            answer += '2'
-    return answer
+    rsp = rsp.replace('2','zero')
+    rsp = rsp.replace('0','five')
+    rsp = rsp.replace('5','two')
+    rsp = rsp.replace('two','2')
+    rsp = rsp.replace('zero','0')
+    rsp = rsp.replace('five','5')
+    return rsp
+    
+    # answer = ''
+    # for i in rsp:
+    #     if i == '2':
+    #         answer += '0'
+    #     elif i == '0':
+    #         answer += '5'
+    #     elif i == '5':
+    #         answer += '2'
+    # return answer
