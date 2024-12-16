376614dfcd7b4a50ab4d0168a475df33e9324365
유종열 <114922306+jongyeol2@users.noreply.github.com>
2024-12-16 09:56:24 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.10 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.py
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.py
@@ -1,7 +1,9 @@
 def solution(n, k):
-    answer = []
-    num = 1
-    while k*num <= n:
-        answer.append(k*num)
-        num += 1
-    return answer
+    # answer = []
+    # num = 1
+    # while k*num <= n:
+    #     answer.append(k*num)
+    #     num += 1
+    # return answer
+
+    return list(range(k,n+1,k))
