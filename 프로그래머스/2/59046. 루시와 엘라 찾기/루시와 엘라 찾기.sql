3c00739503b0e70d9607eba27fd978640619f878
유종열 <114922306+jongyeol2@users.noreply.github.com>
2024-12-17 09:13:06 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,12 @@
+SELECT animal_id,
+       name,
+       sex_upon_intake
+from animal_ins
+where name = 'Lucy'
+   or name = 'Ella'
+   or name = 'Pickle'
+   or name = 'Rogan'
+   or name = 'Sabrina'
+   or name = 'Mitty'
+order by 1
+
