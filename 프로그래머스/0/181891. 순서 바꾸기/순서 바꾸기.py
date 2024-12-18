e82efed2ef5586af1d6be07b6db18943d84339de
유종열 <114922306+jongyeol2@users.noreply.github.com>
2024-12-18 10:50:52 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.py
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.py
@@ -1,7 +1,2 @@
 def solution(num_list, n):
-    answer = []
-    for i in num_list[n:]:
-        answer.append(i)
-    for i in num_list[:n]:
-        answer.append(i)
-    return answer
+    return num_list[n:] + num_list[:n]
