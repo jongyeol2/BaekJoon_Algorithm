16e71f6d48e946100e8ce88c233417ea68e667a2
유종열 <114922306+jongyeol2@users.noreply.github.com>
2024-12-19 10:00:20 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.py
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.py
@@ -2,9 +2,9 @@ def solution(hp):
     a = hp // 5
     b = (hp % 5) // 3
     c = ((hp % 5) % 3) // 1
-    if hp % 5 == 0:
-        return a
-    elif (hp % 5) % 3 == 0:
-        return a + b
-    else:
-        return a + b + c
+    # if hp % 5 == 0:
+    #     return a
+    # elif (hp % 5) % 3 == 0:
+    #     return a + b
+    # else:
+    return a + b + c
