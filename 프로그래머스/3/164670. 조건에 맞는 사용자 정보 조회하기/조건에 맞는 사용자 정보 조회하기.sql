a657c546113c42d42e64ec9d9b906d476da63cff
유종열 <114922306+jongyeol2@users.noreply.github.com>
2024-12-19 09:48:22 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,11 @@
+SELECT u.user_id,
+       u.nickname,
+       concat(u.city,' ',  u.street_address1,' ', coalesce(u.street_address2),'') '전체주소',
+       concat(substring(u.tlno,1,3), '-' , substring(u.tlno,4,4), '-', substring(u.tlno,8)) '전화번호'
+from USED_GOODS_USER u
+     join(select writer_id
+          from USED_GOODS_BOARD
+          group by 1
+          having count(board_id) >= 3
+     ) b on u.user_id = b.writer_id 
+order by 1 desc
