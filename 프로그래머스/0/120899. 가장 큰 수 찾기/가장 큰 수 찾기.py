c031d43301fea5569fcec10cf15a48c7d926fe72
유종열 <114922306+jongyeol2@users.noreply.github.com>
2024-12-20 16:32:28 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
@@ -1,7 +1,2 @@
 def solution(array):
-    answer = []
-    answer.append(max(array))
-    for i in range(len(array)) :
-        if array[i] == max(array):
-            answer.append(i)
-    return answer
+    return [max(array), array.index(max(array))]
