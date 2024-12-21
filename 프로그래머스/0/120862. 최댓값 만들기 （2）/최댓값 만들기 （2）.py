17bf9ad2148d20df66997c2412e54b5f7b1e4397
유종열 <114922306+jongyeol2@users.noreply.github.com>
2024-12-21 16:54:25 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,5 +1,5 @@
 def solution(numbers):
-    sort_num = sorted(numbers)
-    answer = max(sort_num[-1]*sort_num[-2], sort_num[0]*sort_num[1])
+    numbers.sort()
+    answer = max(numbers[-1]*numbers[-2], numbers[0]*numbers[1])
         
     return answer
