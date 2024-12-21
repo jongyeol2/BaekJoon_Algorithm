9f1d47cb9ee94bee840ceafa6c5b3d0dd92eaf45
유종열 <114922306+jongyeol2@users.noreply.github.com>
2024-12-21 17:30:16 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,6 +1,4 @@
 def solution(my_string, num1, num2):
     my_string = list(my_string)
-    str1 = my_string[num1]
-    my_string[num1] = my_string[num2]
-    my_string[num2] = str1
+    my_string[num1], my_string[num2] = my_string[num2], my_string[num1]
     return ''.join(my_string)
