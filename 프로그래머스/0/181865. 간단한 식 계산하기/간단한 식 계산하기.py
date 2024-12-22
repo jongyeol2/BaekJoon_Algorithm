5df30b4ac75c1b60149bff0703c0c206dcd8a9a3
유종열 <114922306+jongyeol2@users.noreply.github.com>
2024-12-22 17:53:55 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -1,10 +1,11 @@
 def solution(binomial):
-    list = binomial.split()
-    if list[1] == '+':
-        return int(list[0]) + int(list[2])
-    if list[1] == '-':
-        return int(list[0]) - int(list[2])
-    if list[1] == '*':
-        return int(list[0]) * int(list[2])
+    return eval(binomial)
+#     list = binomial.split()
+#     if list[1] == '+':
+#         return int(list[0]) + int(list[2])
+#     if list[1] == '-':
+#         return int(list[0]) - int(list[2])
+#     if list[1] == '*':
+#         return int(list[0]) * int(list[2])
     
-    return list
+#     return list
