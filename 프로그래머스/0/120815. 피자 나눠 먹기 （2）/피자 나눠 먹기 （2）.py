c81310461915abc53236b5b4df2bca4bd310582c
유종열 <114922306+jongyeol2@users.noreply.github.com>
2024-12-25 16:09:02 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -1,12 +1,19 @@
+import math
+
 def solution(n):
-    multiple_6 = set()
-    multiple_n = set()
+    return abs(n*6) // math.gcd(6,n) / 6
     
-    for i in range(1, n+1):
-        multiple_6.add(i*6)
-    for i in range(1, 6+1):
-        multiple_n.add(i*n)
     
-    answer = min(multiple_6 & multiple_n) / 6
+    
+    
+#     multiple_6 = set()
+#     multiple_n = set()
+    
+#     for i in range(1, n+1):
+#         multiple_6.add(i*6)
+#     for i in range(1, 6+1):
+#         multiple_n.add(i*n)
+    
+#     answer = min(multiple_6 & multiple_n) / 6
             
-    return answer
+#     return answer
