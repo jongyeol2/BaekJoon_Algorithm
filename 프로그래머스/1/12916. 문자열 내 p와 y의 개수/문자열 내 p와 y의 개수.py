6d476b9bf5966c599c5f6bc390b68f497e49764f
유종열 <114922306+jongyeol2@users.noreply.github.com>
2024-12-27 09:34:02 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.py
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.py
@@ -1,12 +1,14 @@
 def solution(s):
-    sum_p = 0
-    sum_y = 0
-    for i in s:
-        if i in ['p','P']:
-            sum_p += 1
+    
+    return s.lower().count('p') == s.lower().count('y')
+#     sum_p = 0
+#     sum_y = 0
+#     for i in s:
+#         if i in ['p','P']:
+#             sum_p += 1
             
-    for i in s:
-        if i in ['y', 'Y']:
-            sum_y += 1
+#     for i in s:
+#         if i in ['y', 'Y']:
+#             sum_y += 1
 
-    return sum_p == sum_y
+#     return sum_p == sum_y
