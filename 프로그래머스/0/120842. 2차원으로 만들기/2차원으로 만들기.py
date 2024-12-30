2845add97e39eec62ed0eca7640720c9236abbb2
유종열 <114922306+jongyeol2@users.noreply.github.com>
2024-12-30 22:25:54 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -1,7 +1,7 @@
 def solution(num_list, n):
     answer = []
     
-    for i in range(0,int(len(num_list)/n)):
+    for i in range(int(len(num_list)/n)):
         answer.append(num_list[n*i:n*(i+1)])
 
     return answer
