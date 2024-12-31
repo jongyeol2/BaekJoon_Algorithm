831658f23f9130773aa3ee5d97355a0dad9183b1
유종열 <114922306+jongyeol2@users.noreply.github.com>
2024-12-31 10:53:51 +0900

    [level 4] Title: 5월 식품들의 총매출 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
+++ 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
@@ -1,4 +1,3 @@
--- 코드를 입력하세요
 SELECT fp.PRODUCT_ID,
        fp.PRODUCT_NAME,
        fp.PRICE * SUM(fo.AMOUNT)     
@@ -7,5 +6,3 @@ join FOOD_ORDER fo on fp.PRODUCT_ID = fo.PRODUCT_ID
 where fo.PRODUCE_DATE like '2022-05%'
 GROUP BY 1
 ORDER BY 3 DESC, 1
-
-
