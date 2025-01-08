c9fbe52448ed519876bd93ebbe2b15151777476f
유종열 <114922306+jongyeol2@users.noreply.github.com>
2025-01-08 16:48:37 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,25 +1,11 @@
-# def solution(cards1, cards2, goal):
-#     num1 = 0
-#     num2 = 0
-#     for i in goal:
-#         if i == cards1[num1]:
-#             num1 += 1
-#         elif i == cards2[num2]:
-#             num2 += 1
-#         else:
-#             return "No"
-#     return "Yes"
-
 def solution(cards1, cards2, goal):
-    num1, num2 = 0, 0
+    num1 = 0
+    num2 = 0
     for i in goal:
-        # cards1의 현재 단어가 i와 일치하는지 확인
-        if num1 < len(cards1) and cards1[num1] == i:
+        if num1 < len(cards1) and i == cards1[num1]:
             num1 += 1
-        # cards2의 현재 단어가 i와 일치하는지 확인
-        elif num2 < len(cards2) and cards2[num2] == i:
+        elif num2 < len(cards2) and i == cards2[num2]:
             num2 += 1
-        # 어느 쪽에서도 사용 불가하면 "No" 반환
         else:
             return "No"
     return "Yes"
