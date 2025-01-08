93f8bdbcdb2a8831c0d3bf0e8d27c5efeab2961e
유종열 <114922306+jongyeol2@users.noreply.github.com>
2025-01-09 02:15:49 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.12 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -0,0 +1,21 @@
+def solution(n, arr1, arr2):
+    answer = []
+    bin_arr1 = []
+    bin_arr2 = []
+    for i in arr1:
+        bin_i = bin(i)[2:].zfill(n)
+        bin_arr1.append(bin_i)
+    for i in arr2:
+        bin_i = bin(i)[2:].zfill(n)
+        bin_arr2.append(bin_i)
+        
+    for b1, b2 in zip(bin_arr1, bin_arr2):
+        string = ""
+        for i, j in zip(b1, b2):
+            if int(i)+int(j) > 0:
+                string += "#"
+            else:
+                string += " "
+        answer.append(string)
+        
+    return answer
