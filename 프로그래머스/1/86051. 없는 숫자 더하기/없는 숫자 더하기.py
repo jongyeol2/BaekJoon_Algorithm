d2669a2a1999db8cbeccd8c2fda12d35000681bf
유종열 <114922306+jongyeol2@users.noreply.github.com>
2025-01-11 20:14:46 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,7 +1,7 @@
 def solution(numbers):
     num_0to9 = {0,1,2,3,4,5,6,7,8,9}
-    numbers = set(numbers)
-    answer_list = num_0to9.difference(numbers)
+    numbers_set = set(numbers)
     
-    answer = sum(answer_list)
+    answer_set = num_0to9 - numbers_set
+    answer = sum(answer_set)
     return answer
