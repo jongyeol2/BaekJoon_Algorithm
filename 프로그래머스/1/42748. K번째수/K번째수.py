71d2cff36fdcb1e95507c8b1c8fdefa63571d57b
유종열 <114922306+jongyeol2@users.noreply.github.com>
2025-01-27 09:13:23 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,7 @@
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        i, j, k = command
+        num = sorted(array[i-1:j])[k-1]
+        answer.append(num)
+    return answer
