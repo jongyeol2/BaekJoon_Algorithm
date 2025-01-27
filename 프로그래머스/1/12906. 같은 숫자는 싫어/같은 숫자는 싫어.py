0aa19cbe825ed0464c69d85a41ff3be23a02ed92
유종열 <114922306+jongyeol2@users.noreply.github.com>
2025-01-27 09:46:21 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 54.74 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,6 +1,6 @@
 def solution(arr):
     answer = [arr[0]]
-    for i in range(len(arr)-1):
-        if arr[i] != arr[i+1]:
-            answer.append(arr[i+1])
+    for i in arr:
+        if i != answer[-1]:
+            answer.append(i)
     return answer
