9efdc7227eeb9a32b504f722d65e189a67099dba
유종열 <114922306+jongyeol2@users.noreply.github.com>
2025-02-12 09:21:41 +0900

    [level 0] Title: 소인수분해, Time: 0.70 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = []
+    
+    for i in range(2,n+1):
+        while n % i == 0:
+            if i not in answer:
+                answer.append(i)
+            n //= i
+        if n == 1:
+            break
+    return answer
