f7fa78850900ea04d23624ac91e3c64c8aca2576
유종열 <114922306+jongyeol2@users.noreply.github.com>
2025-02-17 09:55:30 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,12 +1,8 @@
 def solution(lottos, win_nums):
-    zero = 0
-    num = 0
     num_dict = {6: 1, 5: 2, 4: 3, 3: 4, 2: 5, 1: 6, 0: 6}
-    for i in lottos:
-        if i:
-            if i in win_nums:
-                num += 1
-        else:
-            zero += 1
+    
+    num = len(set(lottos) & set(win_nums))
+    zero = lottos.count(0)
+              
     answer = [num_dict[num+zero], num_dict[num]]
     return answer
