6bf65b80bc94b18e67e52a2982e54de87c5a137d
유종열 <114922306+jongyeol2@users.noreply.github.com>
2025-11-05 10:31:22 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.05 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -0,0 +1,8 @@
+def solution(lines):
+    set1 = set(range(lines[0][0], lines[0][1]))
+    set2 = set(range(lines[1][0], lines[1][1]))
+    set3 = set(range(lines[2][0], lines[2][1]))
+    
+    overlap = (set1 & set2) | (set1 & set3) | (set2 & set3)          
+    
+    return len(overlap)
